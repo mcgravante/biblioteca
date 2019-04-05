cc8d1658e7ffcc9717d497ba4cf85a1318ab3d57
mgravante <matiasgravante@gmail.com>
2019-04-04 21:00:51 -0300

    Ejercicios de arrays

--- Clase4/main.c
+++ Clase4/main.c
@@ -1,12 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "utn.h"
-#define TAM_ARRAY 3
+#define TAM_ARRAY 10
 
 int main()
 {
 int i;
-int array[TAM_ARRAY]={5,4,0};
-return 0;
+int array[TAM_ARRAY];
+utn_getArrayInt(array,TAM_ARRAY,"Ingrese numeros","Error",99,1,3);
+printf("Array: \n");
+for(i=0;i<TAM_ARRAY;i++){
+printf("%d\n",array[i]);
+};
+float promedio=0;
+utn_calcularPromedio(array,TAM_ARRAY,&promedio);
+printf("Promedio: %.2f\n", promedio);
 
+utn_ordenarArrayMayorMenor(array,TAM_ARRAY);
+printf("Array ordenado: \n");
+for(i=0;i<TAM_ARRAY;i++){
+printf("%d\n",array[i]);
+};
+
+int maximo;
+utn_getMaximo(array,TAM_ARRAY, &maximo);
+printf("Maximo: %d\n",maximo);
+
+int minimo;
+utn_getMinimo(array,TAM_ARRAY, &minimo);
+printf("Minimo: %d\n",minimo);
+
+utn_encontrarNumero(array, TAM_ARRAY, 99, 1, 3, "Ingrese numero a encontrar en el array", "Error");
+return 0;
 }
--- Clase4/utn.c
+++ Clase4/utn.c
@@ -52,7 +52,7 @@ pArrayResultados[i]=buffer;
 return ret;
 }
 
-int calcularPromedio(int *pArrayInt, int limite, float *pResultado){
+int utn_calcularPromedio(int *pArrayInt, int limite, float *pResultado){
 int i;
 int acumulador=0;
 for (i=0; i<limite; i++) {
@@ -62,32 +62,87 @@ acumulador=acumulador + pArrayInt[i];
 return 0;
 }
 
-int swap(int *A, int *B)
-{
-    if ((A!=NULL)&&(B!=NULL)){
-    int *buffer;
-    buffer=A;
-    A=B;
-    B=buffer;
-    return 0;
-    }
-    return -1;
-}
-
-
-
-int ordenarArray(int*pArrayParaOrdenar, int limite)
+int utn_ordenarArrayMenorMayor(int*pArrayParaOrdenar, int limite)
 {
     int flagSwap;
     int i;
+    int buffer;
     do{
     flagSwap=0;
     for(i=0;i<limite-1;i++){
-    if(pArrayParaOrdenar[i]>pArrayParaOrdenar[i-1]){
-    swap(&pArrayParaOrdenar[i],&pArrayParaOrdenar[i-1]);
+    if(pArrayParaOrdenar[i]>pArrayParaOrdenar[i+1]){
+    buffer=pArrayParaOrdenar[i+1];
+    pArrayParaOrdenar[i+1]=pArrayParaOrdenar[i];
+    pArrayParaOrdenar[i]=buffer;
     flagSwap=1;
     }
     }
     }
-    while (flagSwap==1);
+    while (flagSwap == 1);
+    return 0;
+}
+
+int utn_ordenarArrayMayorMenor(int*pArrayParaOrdenar, int limite)
+{
+    int flagSwap;
+    int i;
+    int buffer;
+    do{
+    flagSwap=0;
+    for(i=0;i<limite-1;i++){
+    if(pArrayParaOrdenar[i]<pArrayParaOrdenar[i+1]){
+    buffer=pArrayParaOrdenar[i+1];
+    pArrayParaOrdenar[i+1]=pArrayParaOrdenar[i];
+    pArrayParaOrdenar[i]=buffer;
+    flagSwap=1;
+    }
+    }
+    }
+    while (flagSwap == 1);
+    return 0;
+}
+
+int utn_getMaximo(int *pArrayInt, int limite, int *pMaximo){
+int i;
+int maximo=pArrayInt[0];
+for (i=0; i<limite; i++) {
+if (pArrayInt[i]>maximo){
+maximo=pArrayInt[i];
+}
+}
+*pMaximo=maximo;
+return 0;
+}
+
+int utn_getMinimo(int *pArrayInt, int limite, int *pMinimo){
+int i;
+int minimo=pArrayInt[0];
+for (i=0; i<limite; i++) {
+if (pArrayInt[i]<minimo){
+minimo=pArrayInt[i];
+}
+}
+*pMinimo=minimo;
+return 0;
+}
+
+int utn_encontrarNumero (   int *pArrayInt,
+                            int limite,
+                            int max,
+                            int min,
+                            int reintentos,
+                            char* msg,
+                            char* msgError)
+{
+    int i;
+    int numeroABuscar;
+    utn_getNumber(&numeroABuscar, max, min, reintentos, msg, msgError);
+    for (i=0; i<limite; i++) {
+        if (pArrayInt[i]==numeroABuscar){
+            printf("Se encontró el número %d", numeroABuscar);
+            return 0;
+        }
+    }
+    printf("No se encontró el número %d", numeroABuscar);
+    return -1;
 }
